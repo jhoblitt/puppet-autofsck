5cb958a829badc8c237f07fe9b2ef995d69342db
Joshua Hoblitt <jhoblitt@cpan.org>
2013-03-03 19:02:39 -0700

    fix negative tests

--- spec/classes/autofsck_spec.rb
+++ spec/classes/autofsck_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe 'autofsck' do
   let(:title) { 'redhat' }
   let(:facts) { {:osfamily=> 'RedHat'} }
-
-  it { should include_class('autofsck') }
-
   # include autofsck
-  it do
-    should contain_file('/etc/sysconfig/autofsck').with({
-      'ensure' => 'present',
-      'owner'  => 'root',
-      'group'  => 'root',
-      'mode'   => '0644',
-    })
+  context 'default params' do
+    it do
+      should include_class('autofsck')
+      should contain_file('/etc/sysconfig/autofsck').with({
+        'ensure' => 'present',
+        'owner'  => 'root',
+        'group'  => 'root',
+        'mode'   => '0644',
+      })
+    end
   end
 
 
@@ -22,44 +22,45 @@ describe 'autofsck' do
     let(:params) { {:ensure => 'present'} }
 
     it do
+      should include_class('autofsck')
       should contain_file('/etc/sysconfig/autofsck') 
     end
   end
 
-#  can't figure out how to test for absess from the catalog.  Neither "should
-#  not" or "should !" actually work.
-#
-#  # class{ autofsck: ensure => absent }
-#  context 'with ensure => absent' do
-#    let(:params) { {:ensure => 'absent'} }
-#
-#    it do
-#      should contain_file('/etc/sysconfig/autofsck') 
-#    end
-#  end
+  # class{ autofsck: ensure => absent }
+  context 'with ensure => absent' do
+    let(:params) { {:ensure => 'absent'} }
+
+    it do
+      should include_class('autofsck')
+      should contain_file('/etc/sysconfig/autofsck').with({
+        'ensure' => 'absent',
+      })
+    end
+  end
 
   # invalid arg param
   # class{ autofsck: ensure => foo }
-#   context 'with ensure => foo' do
-#     let(:params) { {:bar => 'foo'} }
-# 
-#     it do
-#       expect {
-#         include_class('autofsck') 
-#       }.to raise_error(Puppet::ParseError, /invalid parameter/)
-#     end
-#   end
-# 
-#   # fail on unsupported osfamily
-#   context 'unsupported osfamily' do
-#     let(:facts) { {:osfamily=> 'Debian'} }
-# 
-#     it do
-#       expect {
-#         include_class('autofsck') 
-#       }.to raise_error(Puppet::ParseError, /Module autofsck is not supported on/)
-#     end
-#   end
+  context 'with ensure => foo' do
+    let(:params) { {:ensure => 'foo'} }
+ 
+    it do
+      expect {
+        should include_class('autofsck') 
+      }.to raise_error(Puppet::Error, /^validate_re\(\)/)
+    end
+  end
+
+  # fail on unsupported osfamily
+  context 'unsupported osfamily' do
+    let(:facts) { {:osfamily=> 'Debian'} }
+ 
+    it do
+      expect {
+        should include_class('autofsck') 
+      }.to raise_error(Puppet::Error, /^Module autofsck is not supported on/)
+    end
+  end
 
 end
 
