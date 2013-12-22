f4ef10c2e920628f15284ac3f3ab29c60b76ed86
Joshua Hoblitt <jhoblitt@cpan.org>
2013-12-22 13:09:03 -0800

    add rspec-puppet-augeas test coverage of Debian augeas resource

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,6 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-augeas'
+
+RSpec.configure do |c|
+  c.augeas_fixtures = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'augeas'))
+end
